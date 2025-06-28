a98e5b7b461c4ff5d7ebb9b55a937a85ff0a394e
taiki-F0827 <tfkd1056@gmail.com>
2025-06-28 14:22:48 +0900

    first commit

--- README.md
+++ README.md
@@ -1,2 +1,1 @@
 # python_test
-https://www.loom.com/share/fed4bbeead8a4a57ad7787462f711370
--- example.py
+++ example.py
@@ -1,28 +1,23 @@
-#1
+#1 hello worldの出力
 print("hello world")
-
-#2greet関数の定義
+#2 greet function
 def greet():
     print("こんにちは")
-
 greet()
-
-#3print_name関数
+#3 print_name function
 def print_name(name:str):
     print("私の名前は"+name+"です")
-
 print_name("タイキ")
 
-#4get_greet関数
+#4 get_greet function
 def get_greet():
     return "おはようございます"
-
 greet=get_greet()
 print(greet)
 
-#5add関数
+#5 add function
 def add(a:int,b:int):
     return a+b
-
 add=add(1,2)
 print(add)
+
